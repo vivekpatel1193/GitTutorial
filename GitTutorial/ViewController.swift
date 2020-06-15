567918b1ee2812d5a41175c7e343460cd1a6a274
vivek patel <vivek.patel1193@gmail.com>
2020-06-15 20:01:03 +0530

    vivek sprint2 changes

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
          print("new 13")
+        print("vivek sprint2 changes")
     }
 
 
